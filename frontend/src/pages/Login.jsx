00127c6489cb3c0b175dab7167916dff79078c44
maurolezcano81 <maurolezcano111@gmail.com>
2024-05-02 16:43:32 -0300

    Finish styles login, and fetching data

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,122 @@
+import User from '../assets/Icons/Login/User.png';
+import Invisible from '../assets/Icons/Login/Invisible.png';
+import Enterprise from '../assets/Enterprise.png'
+import {
+    useState
+} from 'react';
+import {
+    Link
+} from 'react-router-dom';
 
-const Login = () =>{
-    return(
+const Login = () => {
+
+    const urlApi = `${process.env.SV_HOST}${process.env.SV_PORT}${process.env.SV_ADDRESS}/signIn`;
+
+    const [error, setError] = useState('');
+
+    const [username, setUsername] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [isCheck, setIsCheck] = useState(null);
+
+    const changeUsername = (e) => {
+        setUsername(e.target.value);
+    }
+
+    const changePwd = (e) => {
+        setPwd(e.target.value);
+    }
+
+    const changeKeepSesion = (e) => {
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+                const response = await fetch(urlApi, {
+                    method: 'POST',
+                    headers:{
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        username: username,
+                        pwd: pwd
+                    })
+                })
+
+            const fetchData = await response.json();
+
+            const error = document.getElementById('errorMessage');
+            if (response.status != 200) {
+                error.classList.replace('success', 'error');
+                error.style.display = 'block';
+                setError(fetchData.message);
+                return;
+            }
+
+            error.classList.replace('error','success');
+            error.style.display = 'block';
+            setError(fetchData.message);
+
+        } catch (e) {
+            console.error(e.name);
+        }
+    }
+
+    return (
         <div className="login__container">
-            asd
+            <div className="login__content">
+                <div className="login-title">
+                    <h2>INICIAR SESIÓN</h2>
+                </div>
+                <div className="login__form__container">
+                    <div className="login__form__content">
+                        <div className="login__form-img">
+                            <img src={Enterprise} alt="" />
+                        </div>
+                        <form onSubmit={handleSubmit} className="login__form">
+                            <div className="login__input-container">
+                                <label>Nombre de usuario o email</label>
+                                <div className="login-input">
+                                    <input
+                                        type="text"
+                                        onChange={changeUsername} />
+                                    <img src={User} alt="Icono de Usuario" />
+                                </div>
+                            </div>
+                            <div className="login__input-container">
+                                <label>Clave</label>
+                                <div className="login-input">
+                                    <input
+                                        type="password"
+                                        onChange={changePwd} />
+                                    <img src={Invisible} alt="Icono de Usuario" />
+                                </div>
+                            </div>
+
+                            <div className="login__input-checkbox">
+                                <input
+                                    type="checkbox"
+                                    onChange={changeKeepSesion}
+                                />
+                                <p>Mantener sesión iniciada</p>
+                            </div>
+
+                            <div id='errorMessage' className="login__error error">
+                                <p>{error}</p>
+                            </div>
+
+                            <div className="login__button">
+                                <button type='submit'>INICIAR</button>
+                            </div>
+
+                            <div className='login__forgot'>
+                                <Link href="#">¿Olvidates tu contraseña? Haz clic aquí.</Link>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define:{
+    'process.env': JSON.stringify(dotenv.config().parsed)
+  }
 })
