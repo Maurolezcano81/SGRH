9284c7ce09215aa5bf23598576b264265f556513
maurolezcano81 <maurolezcano111@gmail.com>
2024-05-31 22:10:49 -0300

    Add occupation, department, address data in userController

--- backend/controllers/Admin/People/UserControllers.js
+++ backend/controllers/Admin/People/UserControllers.js
@@ -2,6 +2,9 @@ import Employee from '../../../models/Admin/People/Employee.js';
 import Entity from '../../../models/Admin/People/Entity.js';
 import User from '../../../models/Admin/People/User.js';
 import Profile from '../../../models/Auth/Profile.js';
+import Contact from '../../../models/Admin/System/Contact.js';
+import EntityContact from '../../../models/Admin/Address/EntityContact.js';
+import City from '../../../models/Admin/Address/City.js';
 
 import {
   isInputEmpty,
@@ -12,17 +15,32 @@ import {
 } from '../../../middlewares/Validations.js';
 
 import { encryptPwd } from '../../../middlewares/Authorization.js';
+import Department from '../../../models/Admin/Department/Department.js';
+import Occupation from '../../../models/Admin/Department/Occupation.js';
 
 const instanceEmployee = new Employee();
 const instanceEntity = new Entity();
 const instanceUser = new User();
 const instanceProfile = new Profile();
+const instanceContact = new Contact();
+const instanceEntityContact = new EntityContact();
+const instanceCity = new City();
+const instanceDepartment = new Department();
+const instanceOccupation = new Occupation();
 
 export const createUser = async (req, res) => {
   try {
     // DESESTRUCTURACIONES
 
-    const { entity_data, employee_data, user_data, value_profile } = req.body;
+    const {
+      entity_data,
+      employee_data,
+      user_data,
+      entity_contact_data,
+      address_data,
+      entity_department_occupation_data,
+      value_profile,
+    } = req.body;
 
     const avatar_url = req.fileUrl;
 
@@ -30,14 +48,21 @@ export const createUser = async (req, res) => {
     const entity_dataInJson = JSON.parse(entity_data);
     const employee_dataInJson = JSON.parse(employee_data);
     const user_dataInJson = JSON.parse(user_data);
-
+    const entity_contact_dataInJson = JSON.parse(entity_contact_data);
+    const address_dataInJson = JSON.parse(address_data);
+    const entity_department_occupationInJson = JSON.parse(entity_department_occupation_data);
 
     const { name_entity, lastname_entity, date_birth_entity, sex_fk, nacionality_fk } = entity_dataInJson;
 
-    console.log(name_entity);
     const { file_employee, date_entry_employee } = employee_dataInJson;
 
     const { username_user, pwd_user } = user_dataInJson;
+
+    const { value_ec, contact_fk } = entity_contact_dataInJson;
+
+    const { description_address, city_fk } = address_dataInJson;
+
+    const { department_fk, occupation_fk } = entity_department_occupationInJson;
 
     // DESESTRUCTURACIONES
 
@@ -72,6 +97,21 @@ export const createUser = async (req, res) => {
       throw new Error('Debes completar todos los campos de usuario');
     }
 
+    // VALIDACIONES CONTACTO
+    if (isInputEmpty(value_ec) || isInputEmpty(contact_fk)) {
+      throw new Error('Debes completar todos los campos de contacto');
+    }
+
+    // VALIDACIONES DIRECCION
+    if (isInputEmpty(description_address) || isInputEmpty(city_fk)) {
+      throw new Error('Debes completar todos los campos de direccion');
+    }
+
+    // VALIDACIONES ENTITY_DEPARTMENT_OCCUPATION
+    if(isInputEmpty(department_fk) || isInputEmpty(occupation_fk)){
+      throw new Error("Debes completar todos los campos del puesto de trabajo");
+    }
+
     // COMPROBACIONES SI EXISTEN EN BD
 
     const checkExistFileEmployee = await instanceEmployee.getEmployee(file_employee);
@@ -85,6 +125,31 @@ export const createUser = async (req, res) => {
     if (checkExistUser.length > 0) {
       throw new Error('Este nombre de usuario ya esta siendo utilizado');
     }
+
+    const checkExistContact = await instanceContact.getContact(contact_fk);
+
+    if (checkExistContact.length < 1) {
+      throw new Error('Este tipo de contacto no existe');
+    }
+
+    const checkExistCity = await instanceCity.getCityById(city_fk);
+
+    if (checkExistCity.length < 1) {
+      throw new Error('Esta ciudad no existe, ingrese una valida');
+    }
+
+    const checkExistDepartment = await instanceDepartment.getDepartmentById(department_fk);
+
+    if(checkExistDepartment.length < 1){
+      throw new Error("Este departamento no existe, ingrese uno valido")
+    }
+
+    const checkExistOccupation = await instanceOccupation.getOccupationById(occupation_fk);
+
+    if(checkExistOccupation.length < 1){
+      throw new Error("El puesto de trabajo no existe, ingrese uno valido")
+    }
+
 
     // INSERTS EN LA BD
 
@@ -109,6 +174,35 @@ export const createUser = async (req, res) => {
     if (!insertEmployee) {
       throw new Error('Error al crear usuario, comprueba los datos de empleado');
     }
+
+    // CONTACTO
+
+    const entity_contact_data_completed = {
+      value_ec: value_ec,
+      entity_fk: idEntity,
+      contact_fk: contact_fk,
+    };
+
+    const insertEntityContact = await instanceEntityContact.createEntityContact(entity_contact_data_completed);
+
+    if (!insertEntityContact) {
+      throw new Error('Error al crear el usuario, comprueba los datos de contacto');
+    }
+
+    // ADDRESS
+
+    const address_data_completed = {
+      description_address: description_address,
+      city_fk: city_fk,
+      entity_fk: idEntity,
+    };
+
+    const insertAddress = await instanceEntityContact(address_data_completed);
+
+    if (!insertAddress) {
+      throw new Error('Error al crear el usuario, comprueba los datos del domicilio');
+    }
+
     // USUARIO
     const pwdHashed = await encryptPwd(pwd_user);
 
---
+++ backend/models/Admin/Address/EntityContact.js
@@ -0,0 +1,26 @@
+import {database} from "../../../config/database.js";
+import mysql from "mysql2"
+
+class EntityContact {
+    constructor(){
+        this.connection = mysql.createConnection(database)
+    }
+
+    async createEntityContact(entity_contact_data) {
+      try {
+        
+        const {value_ec, entity_fk, contact_fk} = entity_contact_data
+
+        const query = "Insert into entity_contact(value_ec, entity_fk, contact_fk, status_ec, created_at, updated_at) Values(?, ?, ?, 1, now(), now())";
+
+        const [results] = await this.connection.promise().query(query, [value_ec, entity_fk, contact_fk]);
+
+        return results;
+      } catch(error) {
+        console.error(`Error en modelo de EntityController: `+error);
+        throw new Error(`Error al crear el contacto`);
+      }
+    }
+}
+
+export default EntityContact;
