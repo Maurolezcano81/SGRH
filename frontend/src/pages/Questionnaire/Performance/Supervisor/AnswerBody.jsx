f29d6fc4546c9dbddde05999150ce1bfcd56c494
Maurolezcano81 <maurolezcano111@gmail.com>
2024-10-08 23:57:11 -0300

    Fix modal and single quiz page

--- frontend/src/pages/Questionnaire/Performance/Supervisor/AnswerBody.jsx
+++ frontend/src/pages/Questionnaire/Performance/Supervisor/AnswerBody.jsx
@@ -111,7 +111,7 @@ const AnswerBody = ({ ep }) => {
                 setErrorMessage(data.message);
             }
 
-            setListQuestions(data.list);
+            setListQuestions(data.queryResponse);
         };
 
         fetchQuestions();
